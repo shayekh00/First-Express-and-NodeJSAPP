ec25b36c8cc48b9976f78510b8a5caafbd48f6ed
Shayekh Navid <shayekhnavid@gmail.com>
2020-06-08 21:16:33 +0600

    Postgres

---
+++ config/config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  development: {
+    username: "express-mvp-dbuser",
+    password: 12345,
+    database: "express-mvp-db",
+    host: "127.0.0.1",
+    dialect: "postgres",
+    operatorsAliases: false,
+    port: 5432,
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
+    operatorsAliases: false,
+  },
+  production: {
+    username: "root",
+    password: null,
+    database: "database_production",
+    host: "127.0.0.1",
+    dialect: "mysql",
+    operatorsAliases: false,
+  },
+};
