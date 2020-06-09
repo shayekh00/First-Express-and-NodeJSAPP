e571bb9a079b0ffa3000a9bd997c0d5dd5f20a82
Shayekh Navid <shayekhnavid@gmail.com>
2020-06-09 12:46:04 +0600

    leads with email

--- controllers/landing.js
+++ controllers/landing.js
@@ -10,6 +10,13 @@ exports.submit_lead = function (req, res, next) {
   // res.redirect("/");
 
   return models.Lead.create({ email: req.body.lead_email }).then((lead) => {
-    res.redirect("/");
+    res.redirect("/leads");
   });
+};
+
+exports.show_leads = function (req, res, next) {
+  models.Lead.findAll().then(leads => {
+    res.render('landing', { title: 'Express', leads: leads });
+  })
+
 };
